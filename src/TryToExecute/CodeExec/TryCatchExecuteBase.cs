ebf2a18cd981850c4fbafdc08836c01dc017bc31
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-01-21 20:18:31 +0200

    Add methods in the base class to set variables to default value.

--- src/TryToExecute/CodeExec/TryCatchExecuteBase.cs
+++ src/TryToExecute/CodeExec/TryCatchExecuteBase.cs
@@ -4,7 +4,7 @@
 //  Created On       : 2024-12-04 14:52
 // 
 //  Last Modified By : RzR
-//  Last Modified On : 2024-12-04 14:53
+//  Last Modified On : 2025-01-16 21:52
 // ***********************************************************************
 //  <copyright file="TryCatchExecuteBase.cs" company="RzR SOFT & TECH">
 //   Copyright © RzR. All rights reserved.
@@ -14,14 +14,92 @@
 //  </summary>
 // ***********************************************************************
 
+#region U S A G E S
+
+using TryToExecute.Helpers;
+
+#endregion
+
 namespace TryToExecute.CodeExec
 {
     /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     A try catch execute base.
+    /// </summary>
     /// <content>
     ///     A try catch execute base.
     /// </content>
     /// =================================================================================================
     public abstract partial class TryCatchExecuteBase
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Sets to default if present.
+        /// </summary>
+        /// <typeparam name="T1">Generic type parameter.</typeparam>
+        /// <param name="item">[in,out] The item.</param>
+        /// =================================================================================================
+        protected virtual void SetToDefaultIfPresent<T1>(ref T1 item)
+            => RefTypeParamHelper.ToDefaultValue(ref item);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Sets to default if present.
+        /// </summary>
+        /// <typeparam name="T1">Generic type parameter.</typeparam>
+        /// <typeparam name="T2">Generic type parameter.</typeparam>
+        /// <param name="item1">[in,out] The first item.</param>
+        /// <param name="item2">[in,out] The second item.</param>
+        /// =================================================================================================
+        protected virtual void SetToDefaultIfPresent<T1, T2>(ref T1 item1, ref T2 item2)
+            => RefTypeParamHelper.ToDefaultValue(ref item1, ref item2);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Sets to default if present.
+        /// </summary>
+        /// <typeparam name="T1">Generic type parameter.</typeparam>
+        /// <typeparam name="T2">Generic type parameter.</typeparam>
+        /// <typeparam name="T3">Generic type parameter.</typeparam>
+        /// <param name="item1">[in,out] The first item.</param>
+        /// <param name="item2">[in,out] The second item.</param>
+        /// <param name="item3">[in,out] The third item.</param>
+        /// =================================================================================================
+        protected virtual void SetToDefaultIfPresent<T1, T2, T3>(ref T1 item1, ref T2 item2, ref T3 item3)
+            => RefTypeParamHelper.ToDefaultValue(ref item1, ref item2, ref item3);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Sets to default if present.
+        /// </summary>
+        /// <typeparam name="T1">Generic type parameter.</typeparam>
+        /// <typeparam name="T2">Generic type parameter.</typeparam>
+        /// <typeparam name="T3">Generic type parameter.</typeparam>
+        /// <typeparam name="T4">Generic type parameter.</typeparam>
+        /// <param name="item1">[in,out] The first item.</param>
+        /// <param name="item2">[in,out] The second item.</param>
+        /// <param name="item3">[in,out] The third item.</param>
+        /// <param name="item4">[in,out] The fourth item.</param>
+        /// =================================================================================================
+        protected virtual void SetToDefaultIfPresent<T1, T2, T3, T4>(ref T1 item1, ref T2 item2, ref T3 item3, ref T4 item4)
+            => RefTypeParamHelper.ToDefaultValue(ref item1, ref item2, ref item3, ref item4);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Sets to default if present.
+        /// </summary>
+        /// <typeparam name="T1">Generic type parameter.</typeparam>
+        /// <typeparam name="T2">Generic type parameter.</typeparam>
+        /// <typeparam name="T3">Generic type parameter.</typeparam>
+        /// <typeparam name="T4">Generic type parameter.</typeparam>
+        /// <typeparam name="T5">Generic type parameter.</typeparam>
+        /// <param name="item1">[in,out] The first item.</param>
+        /// <param name="item2">[in,out] The second item.</param>
+        /// <param name="item3">[in,out] The third item.</param>
+        /// <param name="item4">[in,out] The fourth item.</param>
+        /// <param name="item5">[in,out] The fourth item.</param>
+        /// =================================================================================================
+        protected virtual void SetToDefaultIfPresent<T1, T2, T3, T4, T5>(ref T1 item1, ref T2 item2, ref T3 item3, ref T4 item4, ref T5 item5)
+            => RefTypeParamHelper.ToDefaultValue(ref item1, ref item2, ref item3, ref item4, ref item5);
     }
 }
